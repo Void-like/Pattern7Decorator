084720338a46574899907b79d12ce734399f7c0c
Void <gloh4520@gmail.com>
2026-03-16 15:11:49 +0300

    бульбазавр

--- Pattern7Decorator/Program.cs
+++ Pattern7Decorator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Pattern7Decorator.ClassDiagramDescription;
 using Pattern7Decorator.ConcreteDecorators;
+using System;
 using System.Text;
 
 namespace Pattern7Decorator
@@ -10,26 +11,46 @@ namespace Pattern7Decorator
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
-            Console.WriteLine("Арсенал Магического Кузнеца ");
-             
-            Console.WriteLine(" Базовое оружие ");
+            Console.WriteLine("Арсенал Магического Кузнеца ");         
+            Console.WriteLine("1.Взять новый меч ");
+            Console.WriteLine("2.Заточка ");
+            Console.WriteLine("3.Руна Огня ");
+            Console.WriteLine("4.Вампиризм  ");
+            Console.WriteLine("5.Тест ");
+            Console.WriteLine("6.Выход ");
+           
             IWeapon sword = new BaseSword("Стальной меч", 20, 100m);
-            PrintWeapon(sword);
-            
-            Console.WriteLine("После заточки");
-            IWeapon sharpenedSword = new SharpeningDecorator(sword);
-            PrintWeapon(sharpenedSword);
+            while (true) {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                         sword = new BaseSword("Стальной меч", 20, 100m);
+                        PrintWeapon(sword);
+                        break;
+                    case "2":
+                        sword = new SharpeningDecorator(sword);
+                        PrintWeapon(sword);
+                        break;
+                    case "3":
+                        sword = new FireRuneDecorator(sword);
+                        PrintWeapon(sword);
+                        break;
+                    case "4":
+                        sword = new VampirismEnchantDecorator(sword);
+                        PrintWeapon(sword);
+                        break;
+                    case "5":
+                        Test();
+                        break;
+                    case "6":
+                        return;
+                    default:
+                        Console.WriteLine("Бред не пиши");
+                        break;
+                }
 
-    
-            Console.WriteLine("После заточки и Руны Огня ");
-            IWeapon fireSharpenedSword = new FireRuneDecorator(sharpenedSword);
-            PrintWeapon(fireSharpenedSword);
- 
-            Console.WriteLine("Другой порядок Вампиризм -> Заточка");
-            IWeapon newSword = new BaseSword("Стальной меч", 20, 100m);
-            IWeapon vampiricSword = new VampirismEnchantDecorator(newSword);
-            IWeapon sharpenedVampiricSword = new SharpeningDecorator(vampiricSword);
-            PrintWeapon(sharpenedVampiricSword);
+            }
 
         }
 
@@ -38,6 +59,27 @@ namespace Pattern7Decorator
             Console.WriteLine($"Предмет: {weapon.GetDescription()}");
             Console.WriteLine($"Урон: {weapon.GetDamage()}");
             Console.WriteLine($"Цена: {weapon.GetPrice()}");
+        }
+        static void Test()
+        {
+            Console.WriteLine(" Базовое оружие ");
+            IWeapon sword = new BaseSword("Стальной меч", 20, 100m);
+            PrintWeapon(sword);
+
+            Console.WriteLine("После заточки");
+            IWeapon sharpenedSword = new SharpeningDecorator(sword);
+            PrintWeapon(sharpenedSword);
+
+
+            Console.WriteLine("После заточки и Руны Огня ");
+            IWeapon fireSharpenedSword = new FireRuneDecorator(sharpenedSword);
+            PrintWeapon(fireSharpenedSword);
+
+            Console.WriteLine("Другой порядок Вампиризм -> Заточка");
+            IWeapon newSword = new BaseSword("Стальной меч", 20, 100m);
+            IWeapon vampiricSword = new VampirismEnchantDecorator(newSword);
+            IWeapon sharpenedVampiricSword = new SharpeningDecorator(vampiricSword);
+            PrintWeapon(sharpenedVampiricSword);
         }
     }
 }
